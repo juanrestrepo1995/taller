212d3086c07c0010bc63e507552be98937752d82
juanrestrepo1995 <62155773+juanrestrepo1995@users.noreply.github.com>
2020-06-12 12:03:59 -0500

    taller web1
    elaboracion del taller de web1

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+/*
+// mi primer codigo PHP
+//quemando texto 
+//variables tipo alfanumericos o tipo texto
+$nombre="Juan Fernando";
+$apellidos="Restrepo Gonzalez ";
+$direccion="cra 14# 14-41 int 201";
+$ciudad="girardota";
+$departamento="antioquia";
+
+//variables tipo dobles o desimales 
+$estatura=1.72;
+
+//variables tipo numericos o enteros 
+$edad=25;
+$documento=1035870254;
+
+//imprimiendo datos
+echo("señor usuario, su nombre es " .$nombre);
+echo("<br>");
+echo("<br>");
+echo("senor ususario sus apellidos son "   .$apellidos);
+echo("<br>");
+echo("<br>");
+echo("la direccion de ".$nombre." ".$apellidos." es:".$direccion );
+*/
+
+//calculadora 
+/*****************************************************
+ * programa que sirve de calculadora aritmetica 
+ * programado por: Juan Fernando Restrepo Gonzalez 
+ * ***********************************************/
+/*
+ $numero1=65;
+ $numero2=34;
+
+ $suma=$numero1+$numero2;
+ $resta=$numero1-$numero2;
+ $multiplicacion=$numero1*$numero2;
+ $divicion=$numero1/$numero2;
+
+
+ echo("claculadora 5000 lo ultimo del coronavirus");
+ echo("<br>");
+ echo("<br>");
+ echo("...........................................");
+ echo("<br>");
+ echo("<br>");
+ echo("que comiense el procimo nivel");
+ echo("<br>");
+ echo("<br>");
+
+ echo("la suma de: ".$numero1."+".$numero2."=".$suma);
+ echo("<br>");
+ echo("la resta de: ".$numero1."-".$numero2."=".$resta);
+ echo("<br>");
+ echo("la multiplicacion de: ".$numero1."*".$numero2."=".$multiplicacion);
+ echo("<br>");
+ echo("la multiplicacion de: ".$numero1."/".$numero2."=".$divicion);*/
+
+
+// segundo ejercicio
+
+// Almacenar el IMC de cada persona
+//gimnasio bodytech//
+/*$peso =67;
+$altura = 1.78;
+$imc = $peso/($altura*$altura);
+
+//condicionales//
+if($imc <= 18.4 ){
+    echo("tu imc es de ".$imc." Tienes insuficiencia de peso");
+
+}else if($imc >= 18.5 && $imc <= 24.9){
+       echo("tu imc es de ".$imc." Tienes un peso normal");
+    
+} else if($imc >= 25 && $imc <= 29.9){
+       echo("tu imc es de ".$imc." tienes sobrepeso");
+    
+} else if($imc >= 30 &&  $imc <= 34.9){
+        echo("tu imc es de ".$imc." Tienes obesidad 1");
+
+    
+} else if($imc >= 35 && $imc <= 39.9){
+        echo("tu imc es de ".$imc." tienes obesidad 2");
+    
+} else if($imc >= 40){
+        echo("tu imc es de ".$imc." tienes obesidad 3");
+    
+
+}*/
+
+
+/*3 descuentos de los zapatos
+$cantidadzapatos=5;
+$valorunitario=150000;
+$totalcompra=$cantidadzapatos*$valorunitario;
+$descuento =$totalcompra*$cantidadzapatos;
+$valorneto = $descuento+$totalcompra;
+
+
+if($cantidadzapatos==3){
+    $descuento=$totalcompra*0.10;
+} else if($cantidadzapatos>3 && $cantidadzapatos <=5){
+    $descuento=$totalcompra*0.20;
+} else if($cantidadzapatos >5){
+    $descuento=$totalcompra*0.40;
+    }
+echo("venta total: ".$cantidadzapatos);
+echo("<br>");
+echo("total de la compra: ".$totalcompra);
+echo("<br>");
+echo("valor total a pagar con descuento ".$valorneto);*/
+
+
+//4 empleado de postobon
+
+  /* $hora = 36;
+   $salario=20000;
+   $horaextra=25000;
+
+    
+    if ($hora <= 40){
+        $salario =20000*$hora;
+        echo("su sueldo semanal sera de " .$salario);
+    } else if($hora > 40){
+        $salario = 25000*$hora;
+        echo("su sueldo semanal con horas extras " .$salario);
+    }*/
+
+
+//5 salpicom
+//areglo indexado
+/*$producto1=array("banano","manzana","durazno","piña","naranja","lulo");
+print_r($producto1);
+echo("<br>");
+//arreglo asociativo
+$productos2=array("dulce1"=>"banano","dulce2"=>"manzana","dulce3"=>"durazno","acido1"=>"piña","acido2"=>"naranja","acido3"=>"lulo");
+print_r($productos2);
+echo("<br>");
+foreach($productos2 as $clave=>$valor){
+    echo($clave);
+    echo("=>");
+    echo($valor);
+
+    echo("<br>");
+
+}
+*/
+
+?>
